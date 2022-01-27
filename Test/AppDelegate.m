f01393b4efd4937046283342ebe38ea9d012abed
wangqiang <wangqiang@kkworld.com>
2022-01-27 10:49:44 +0800

    revert test ,add 1

--- Test/AppDelegate.m
+++ Test/AppDelegate.m
@@ -16,6 +16,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    //1
     return YES;
 }
 
